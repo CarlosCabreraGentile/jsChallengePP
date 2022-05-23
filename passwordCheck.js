7fb770dfd9951b73e08947c09ddc37e7e4fcf00a
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2022-05-23 11:41:06 -0300

    added comment

--- passwordCheck.js
+++ passwordCheck.js
@@ -1,4 +1,6 @@
 function StringChallenge(str) { 
+  // one capital letter, at least one number, one symbol, between 7 and 31 characters, 
+  // cannot have password word
     let passwordCheck = /^(?=.*\d)(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{7,31}$/
     // code goes here  
     if(str === 'password'){
